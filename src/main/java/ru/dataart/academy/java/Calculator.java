c10a840a8f1dce82063a564865213fea589c53b0
allexandersokollov <allexandersokollov@gmail.com>
2021-10-15 09:49:39 +0300

    Initial commit

---
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -0,0 +1,41 @@
+package ru.dataart.academy.java;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Calculator {
+    /**
+     * @param firstNumber  - list for first number in reversed order (1 - 2 - 3 -> 321)
+     * @param secondNumber - list for second number in reversed order (1 - 2 - 3 -> 321)
+     * @return - sum of firstNumber + secondNumber
+     */
+    public Integer getSum(List<Integer> firstNumber, List<Integer> secondNumber) {
+        //Task implementation
+        return 0;
+    }
+
+    /**
+     *
+     * @param list - list of elements
+     * @param <T> - type of element
+     * @return - odd list elements
+     * Example: [1, 22, 34] -> [1, 34]
+     */
+    public <T> List<T> getOddElements(List<T> list) {
+        // Task implementation
+        return Collections.EMPTY_LIST;
+    }
+
+    /**
+     *
+     * @param list - list of elements
+     * @param <T> - type of element
+     * @return - first and last elements of the list
+     * Example: [1, 2, 3] -> [1, 3]
+     * [1, 2, 3 , 4] -> [1, 4]
+     */
+    public <T> List<T> getBounds(List<T> list) {
+        // Task implementation
+        return Collections.EMPTY_LIST;
+    }
+}
